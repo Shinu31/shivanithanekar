778ffab79f55bc2b9e168c9cd5dbbc36cb8f71fe
Shinu <Shinu@Shinu31>
2024-03-14 15:17:22 +0530

    commit

--- HybridActiTimeProject/src/test/TaskTestCase.java
+++ HybridActiTimeProject/src/test/TaskTestCase.java
@@ -1,5 +1,40 @@
 package test;
 
-public class TaskTestCase {
+import java.io.IOException;
 
-}
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+import generic.CustomListener;
+import generic.Flib;
+import Page.HomePage;
+import Page.LoginPage;
+import Page.TasksPage;
+@Listeners(CustomListener.class)
+
+public class TaskTestCase extends BaseTest {
+	
+
+	 
+	  @Test
+	  public void project_customerMethod() throws IOException {
+		  
+		  LoginPage lp = new LoginPage(driver);
+		  Flib flib = new Flib();
+		  lp.validLoginMethod(flib.readPropertyData(PROP_PATH,"Username"),flib.readPropertyData(PROP_PATH,"Password"));
+	      HomePage hp = new HomePage(driver);
+	      hp.clickOntaskModule();
+	      // we are there in Task page
+	      TasksPage ts = new TasksPage(driver);
+	      // create the customer.
+	      ts.createNewCustomerMethod(flib.readExcelData(EXCEL_PATH,CUST_PROPSHEET_NAME,1,0));
+	      // Create the project
+	      ts.createNewProjectMethod(flib.readExcelData(EXCEL_PATH,CUST_PROPSHEET_NAME,1,0),flib.readExcelData(EXCEL_PATH,CUST_PROPSHEET_NAME,1,1));
+	  
+	  }
+
+
+	}
+
+
--- wcsa5seleniumproject/src/MethodsOfAction/MoveToElementMethod.java
+++ wcsa5seleniumproject/src/MethodsOfAction/MoveToElementMethod.java
@@ -1,3 +1,4 @@
+
 package MethodsOfAction;
 
 import java.time.Duration;
